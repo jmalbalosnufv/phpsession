9677c2a8d2ae4a44ff942e86b01c9607c8d931d8
jmalbalosnufv <jmalbalosfeb@gmail.com>
2026-01-14 14:59:35 +0800

    Add files via upload

---
+++ phpsession/header.php
@@ -0,0 +1,3 @@
+<header>
+    <h1>PHP Require & Include Example</h1>
+</header>
---
+++ phpsession/login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>Login</title>
+</head>
+<body>
+
+<h2>Login Page</h2>
+<?php if ($error): ?>
+
+<p style="color:red;"><?php echo htmlspecialchars($error); ?></p>
+
+<?php endif; ?>
+<form method="post">
+    <label>Username:</label><br>
+    <input type="text" name="username"><br><br>
+    <label>Password:</label><br>
+
+    <input type="password" name="password"><br><br>
+    <button type="submit">Login</button>
+</form>
+</body>
+</html>
+
+<?php
+
+session_start();
+
+
+// correct credentials​
+
+$correct_username = "kris";
+$correct_password = "1234";
+$error = "";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = trim($_POST['username'] ?? '');
+    $password = trim($_POST['password'] ?? '');
+    if ($username === "" || $password === "") {
+        $error = "Username and Password are required";
+    }
+
+ elseif ($username !== $correct_username || $password !== $correct_password) {
+
+        $error = "Invalid Username or Password";
+
+    }
+    else {
+        $_SESSION['username'] = $username;
+        header("Location: dashboard.php");
+        exit;
+    }
+}
+?>  
